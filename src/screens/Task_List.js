201b897d7b1950e500681fa77091afa3435b14b4
unknown <rodriguesfreitaslucas@gmail.com>
2024-05-21 10:42:10 -0300

    alteração em TaskList

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,6 +1,31 @@
 import React, {Component} from "react"
-import {StyleSheets, View, Text} from "react-native"
+import {StyleSheet, View, Text, ImageBackground} from "react-native"
+
+import today_image from "../../assets/imgs/today.jpg"
 
 export default  class TaskList extends Component{
-    
+    render(){
+        return(
+            <View style={styles.container}>
+                <ImageBackground source={today_image}>
+                    
+                </ImageBackground>
+                <View style={styles.tasklist}>
+
+                </View>               
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background:{
+        flex: 3
+    },
+    tasklist:{
+        flex: 7
+    }
+})
